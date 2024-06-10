bc5bbcfa7f01397fb7d53fa209d7977a3e4c48b7
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-06-10 22:25:19 +0300

    add admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import GoodsModel, CompetitorGoodsModel
+
+
+@admin.register(GoodsModel)
+class GoodsModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "code",
+        "minimal_price",
+    )
+
+
+@admin.register(CompetitorGoodsModel)
+class CompetitorGoodsModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "url",
+    )
