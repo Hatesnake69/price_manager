c7264b957e673ecc2944c98b3d1d94d32af5f6d4
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-06-10 22:37:46 +0300

    fix CompetitorGoodsModel

--- app/models/competitor_goods.py
+++ app/models/competitor_goods.py
@@ -2,4 +2,8 @@ from django.db import models
 
 
 class CompetitorGoodsModel(models.Model):
+
     url = models.URLField(verbose_name='Ссылка на карточку товара', null=False)
+
+    def __str__(self):
+        return self.url
