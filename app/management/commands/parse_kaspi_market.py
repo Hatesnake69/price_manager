fdb1123288d9539455f0b61cd951d9096d7cf82a
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-07-22 11:36:02 +0300

    add parse_kaspi_market cmd

--- app/management/commands/parse_kaspi_market.py
+++ app/management/commands/parse_kaspi_market.py
@@ -19,7 +19,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")  # Запуск в безголовом режиме
+        chrome_options.add_argument("--headless")  # Запуск в безголовом режиме
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
