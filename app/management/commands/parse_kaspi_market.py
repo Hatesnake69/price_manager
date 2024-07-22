d24133f702583209fc145b3e5dc103c75319ceee
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-07-23 00:39:01 +0300

    add more logs

--- app/management/commands/parse_kaspi_market.py
+++ app/management/commands/parse_kaspi_market.py
@@ -19,22 +19,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-infobars")
-        chrome_options.add_argument("--disable-popup-blocking")
-        chrome_options.add_argument("--disable-translate")
-        chrome_options.add_argument("--disable-web-security")
-        chrome_options.add_argument("--ignore-certificate-errors")
-        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--headless")  # Если нужно запускать браузер в фоновом режиме
+        chrome_options.add_argument("--disable-gpu")  # Отключение GPU для совместимости
 
+        # Добавление заголовков
         chrome_options.add_argument(
-            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-        )
+            "user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0")
+        chrome_options.add_argument(
+            "accept=text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8")
+        chrome_options.add_argument("accept-language=en-US,en;q=0.5")
+        chrome_options.add_argument("accept-encoding=gzip, deflate, br, zstd")
+        chrome_options.add_argument("connection=keep-alive")
+        chrome_options.add_argument(
+            "cookie=ks.tg=9; k_stat=7f6bc9ec-d0cf-40d7-a919-6fc5e622c2ab; kaspi.storefront.cookie.city=750000000; current-action-name=Index")
+        chrome_options.add_argument("upgrade-insecure-requests=1")
+        chrome_options.add_argument("sec-fetch-dest=document")
+        chrome_options.add_argument("sec-fetch-mode=navigate")
+        chrome_options.add_argument("sec-fetch-site=none")
+        chrome_options.add_argument("sec-fetch-user=?1")
+        chrome_options.add_argument("priority=u=0, i")
         chrome_options.binary_location = "/usr/bin/google-chrome"
         driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()),
