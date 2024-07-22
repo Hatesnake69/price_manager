c423f416f7a38faf84f041dec835da29e07e8352
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-07-22 11:11:24 +0300

    add parse_kaspi_market cmd

--- app/management/commands/parse_kaspi_market.py
+++ app/management/commands/parse_kaspi_market.py
@@ -17,8 +17,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Запуск в безголовом режиме
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
         chrome_options.binary_location = "/usr/bin/google-chrome"  # Укажите путь к Chrome
-
         driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()),
             options=chrome_options
