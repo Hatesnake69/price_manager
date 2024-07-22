e419ac72af33c79b5676d3549c0ad687c6d99907
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-07-22 11:19:40 +0300

    add parse_kaspi_market cmd

--- app/management/commands/parse_kaspi_market.py
+++ app/management/commands/parse_kaspi_market.py
@@ -3,11 +3,8 @@ from django.core.management.base import BaseCommand
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-
 
 from app.models import KaspiGoodsModel
 
@@ -27,19 +24,20 @@ class Command(BaseCommand):
             options=chrome_options
         )
 
-
         all_goods: list[KaspiGoodsModel] = KaspiGoodsModel.objects.all()
         for good in all_goods:
             if "_Ledvisionkz" in good.sku:
                 goods_sku = good.sku.split("_")[0]
                 url = f"https://kaspi.kz/shop/search/?text={goods_sku}&q=%3AavailableInZones%3AMagnum_ZONE1&sort=relevance&filteredByCategory=false&sc="
                 driver.get(url)
-                wait = WebDriverWait(driver, 5)
-                first_product = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href*="/shop/p/"]')))
-                # Получение ссылки
-                product_link = first_product.get_attribute('href')
-                good.kaspi_offer_url = product_link
-                good.save()
-                print(f"offer url was added for good {good.model}")
-                print(f"{good.kaspi_offer_url}")
+                try:
+                    first_product = driver.find_element(By.CSS_SELECTOR, 'a[href*="/shop/p/"]')
+                    # Получение ссылки
+                    product_link = first_product.get_attribute('href')
+                    good.kaspi_offer_url = product_link
+                    good.save()
+                    print(f"offer url was added for good {good.model}")
+                    print(f"{good.kaspi_offer_url}")
+                except Exception as e:
+                    print(f"Error occurred for good {good.model}: {e}")
         self.stdout.write(self.style.SUCCESS('Команда выполнена успешно!'))
