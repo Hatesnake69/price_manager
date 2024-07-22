c90aedd26c47211cebf89ed0a018652270588dfa
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-07-22 11:14:23 +0300

    add parse_kaspi_market cmd

--- app/management/commands/parse_kaspi_market.py
+++ app/management/commands/parse_kaspi_market.py
@@ -34,7 +34,7 @@ class Command(BaseCommand):
                 goods_sku = good.sku.split("_")[0]
                 url = f"https://kaspi.kz/shop/search/?text={goods_sku}&q=%3AavailableInZones%3AMagnum_ZONE1&sort=relevance&filteredByCategory=false&sc="
                 driver.get(url)
-                wait = WebDriverWait(driver, 0.1)
+                wait = WebDriverWait(driver, 1)
                 first_product = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href*="/shop/p/"]')))
                 # Получение ссылки
                 product_link = first_product.get_attribute('href')
