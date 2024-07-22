3f854890a9818860ab938510fa2c07c878458c62
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-07-23 00:07:57 +0300

    add more logs

--- app/management/commands/parse_kaspi_market.py
+++ app/management/commands/parse_kaspi_market.py
@@ -19,7 +19,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         chrome_options = Options()
-        chrome_options.add_argument("--headless")  # Запуск в безголовом режиме
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
@@ -31,8 +31,9 @@ class Command(BaseCommand):
         chrome_options.add_argument("--disable-web-security")
         chrome_options.add_argument("--ignore-certificate-errors")
         chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-        chrome_options.binary_location = "/usr/bin/google-chrome"  # Укажите путь к Chrome
+        chrome_options.add_argument(
+            "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+        chrome_options.binary_location = "/usr/bin/google-chrome"
         driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()),
             options=chrome_options
