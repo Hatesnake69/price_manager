15cdaaf4ab54dae9bf7e0d46639265d892625f8d
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-07-23 16:18:52 +0300

    add more logs

--- app/management/commands/parse_kaspi_market.py
+++ app/management/commands/parse_kaspi_market.py
@@ -46,10 +46,7 @@ class Command(BaseCommand):
         driver.get("https://www.google.com")
         print(f"Google page title: {driver.title}")
         driver.get("https://kaspi.kz/shop/search/?text=105509884&q=%3AavailableInZones%3AMagnum_ZONE1&sort=relevance&filteredByCategory=false&sc=")
-        # WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, "item-card__info")))
-        title = driver.execute_script("return document.title;")
-
-        print(f"KASPI page title: {title}")
+        print(f"KASPI page_source: {driver.page_source}")
         all_goods: list[KaspiGoodsModel] = KaspiGoodsModel.objects.all()
         for good in all_goods:
             if "_Ledvisionkz" in good.sku:
