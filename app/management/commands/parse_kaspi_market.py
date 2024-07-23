53b4d3179f49f96e6c55cf7382e6f2bc3a48e18a
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-07-23 17:25:47 +0300

    fix chrome options

--- app/management/commands/parse_kaspi_market.py
+++ app/management/commands/parse_kaspi_market.py
@@ -24,6 +24,7 @@ class Command(BaseCommand):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--remote-debugging-port=9222")
+        chrome_options.binary_location = "/usr/bin/google-chrome"
         driver = webdriver.Chrome(
             options=chrome_options
         )
