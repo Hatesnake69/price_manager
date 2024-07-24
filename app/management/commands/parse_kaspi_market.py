6454f101a45fafe029ea0d79fc4664f5425bee5a
VladimirKhrabrov <khrabrov.v@brandsgo.digital>
2024-07-24 12:09:12 +0300

    fix chrome options

--- app/management/commands/parse_kaspi_market.py
+++ app/management/commands/parse_kaspi_market.py
@@ -24,6 +24,9 @@ class Command(BaseCommand):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--remote-debugging-port=9222")
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+        chrome_options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
         chrome_options.binary_location = "/usr/bin/google-chrome"
         driver = webdriver.Chrome(
             options=chrome_options
